c2604bde60003ab7732195ffa2b51c5d89213a37
Victor Perez <vaperezr@unal.edu.co>
2018-01-31 04:47:51 -0500

    feature: creates an api file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
+import { getComics } from './utils/api'
 import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    getComics().then(response => this.updateComics(response.data))
+  }
+
+  updateComics = (data) => {
+    this.setState({ comics: data.results })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.comics && this.state.comics.map(comic => (
+            <div key={comic.id}>
+              {comic.title}
+            </div>
+          ))
+        }
       </div>
     )
   }
---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import md5 from 'md5'
+import { apikey, privatekey, baseUrl } from './config'
+
+const api = baseUrl
+
+const params = {
+  apikey,
+  ts: Date.now(),
+  hash: md5(Date.now() + privatekey + apikey),
+}
+
+const headers = {
+  Accept: '*/*',
+}
+
+export const getComics = () =>
+  axios.get(`${api}comics`, {
+    params,
+    headers,
+  })
+    .then(data => data.data)
+    .catch(error => console.error(error))
